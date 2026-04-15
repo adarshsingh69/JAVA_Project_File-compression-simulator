3d74f2800626523be4bc85512c9f93f72d828f7f
adarshsingh69 <singhadarsh02082008@gmail.com>
2026-04-15 07:40:10 +0000

    start of the code

---
+++ project.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Compressor {
+
+     String compress(String input) {
+
+        StringBuilder compressed = new StringBuilder();
+        
+
+     String decompress(String input) {
+
+        StringBuilder decompressed = new StringBuilder();
+
+        
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter String: ");
+        String input = sc.nextLine();
+
+        
+        sc.close();
+    }
+}
