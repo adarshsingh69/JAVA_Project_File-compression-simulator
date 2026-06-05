c00b24ed3ecd52f5160100d0f85a717c5b6bc6b9
adarshsingh69 <singhadarsh02082008@gmail.com>
2026-06-05 18:21:28 +0530

    Refactor Compressor class and update output format

--- project.java
+++ project.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Compressor {
+public class Compressor {
 
     public String compress(String str) {
         StringBuilder result = new StringBuilder();
@@ -34,9 +34,6 @@ class Compressor {
 
         return result.toString();
     }
-}
-
-public class CompressionDemo {
 
     public static void main(String[] args) {
 
@@ -50,9 +47,9 @@ public class CompressionDemo {
         String compressed = obj.compress(input);
         String decompressed = obj.decompress(compressed);
 
-        System.out.println("Original : " + input);
-        System.out.println("Compressed : " + compressed);
-        System.out.println("Decompressed : " + decompressed);
+        System.out.println("\nOriginal String: " + input);
+        System.out.println("Compressed String: " + compressed);
+        System.out.println("Decompressed String: " + decompressed);
 
         if (input.equals(decompressed)) {
             System.out.println("Success");
